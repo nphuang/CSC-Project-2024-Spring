c7efc7ec374e52fde6aa5128fc0396c008f11350
PHH <phqq.cs10@nycu.edu.tw>
2024-04-23 21:26:29 +0800

    only need to modify send_spoofed_dns_reply()

--- csc2024-project2/pharm_attack.cpp
+++ csc2024-project2/pharm_attack.cpp
@@ -435,24 +435,28 @@ void send_spoofed_dns_reply(char *packet)
     // packet - ip_header to get the udp datagram
     udp_header->check = 0;
     uint32_t sum = 0;
-    sum += (ip_header->saddr >> 16) & 0xFFFF;
-    sum += ip_header->saddr & 0xFFFF;
-    sum += (ip_header->daddr >> 16) & 0xFFFF;
-    sum += ip_header->daddr & 0xFFFF;
+    // calculate the pseudo header
+    sum += (ip_header->saddr >> 16) + (ip_header->saddr & 0xFFFF);
+    sum += (ip_header->daddr >> 16) + (ip_header->daddr & 0xFFFF);
     sum += htons(IPPROTO_UDP);
     sum += udp_header->len;
-
-    unsigned short *udp_data = (unsigned short *)udp_header;
-    for (int i = 0; i < ntohs(udp_header->len) / 2; i++) {
-        if (&udp_data[i] != &udp_header->check) // 排除校验和字段本身
-            sum += ntohs(udp_data[i]);
+    // calculate the udp datagram
+    unsigned short *udp_checksum = (unsigned short *)(packet + ip_header->ihl * 4);
+    for (int i = 0; i < total_len - ip_header->ihl * 4; i += 2)
+    {
+        uint16_t word = ntohs(udp_checksum[i]);
+        sum += word;
     }
-
-    while (sum >> 16) {
+    if (total_len % 2)
+    {
+        sum += ntohs((uint16_t)(*(packet + total_len - 1) << 8));
+    }
+    while (sum >> 16)
+    {
         sum = (sum & 0xFFFF) + (sum >> 16);
     }
-
     udp_header->check = htons(~sum);
+
 
     // calculate the ip checksum
     ip_header->check = 0;
